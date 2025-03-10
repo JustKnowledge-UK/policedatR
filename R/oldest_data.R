969bd6d7a208c499027075eacb48452e09922e1a
JolyonJoseph <jmileswilson@gmail.com>
2025-03-10 16:38:09 +0000

    add oldest_data and its documentation and import

---
+++ R/oldest_data.R
@@ -0,0 +1,22 @@
+#' Retrieve oldest date for which data in API are available
+#'
+#'
+#' @return A string describing date in yyyy-mm format
+#' @export
+#'
+#'
+oldest_data <- function(){
+  oldest <- httr::GET("https://data.police.uk/api/crimes-street-dates")
+  k <- httr::content(oldest)
+  l <- lapply(k, unlist)
+  m <- do.call(dplyr::bind_rows, l)
+
+  # format as date to be able to use min/max functions
+  for(i in 1:nrow(m)){
+    m$date[i] <- paste0(m$date[i],"-01")
+  }
+  m$date <- as.Date(m$date)
+
+  substr(min(m$date), 1, 7)
+
+}
