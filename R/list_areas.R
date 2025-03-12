e3420f634d11fb63f365f200f2bb1438e8ed243e
JolyonJoseph <jmileswilson@gmail.com>
2025-03-12 16:54:20 +0000

    add v1 of list_areas, its documentation and export

---
+++ R/list_areas.R
@@ -0,0 +1,27 @@
+#' List available areas
+#'
+#' List unique values of the different area types that could be used to get
+#' data from the Police API.
+#'
+#' @param area_type A string definiting the area type for which distinct values
+#' are sought. Options are "oa" (Output Area), "lsoa", (Lower layer Super Output
+#' area), "msoa" (Middle layer Super Output Area), and "lad" (Local Authority
+#' District)
+#'
+#' @returns A character vector of the unique values of area_type.
+#' @export
+#'
+#' @examples
+#'
+#' list_areas(area_type = "lad")
+
+list_areas <- function(area_type = "lad"){
+  area_dict <- list("oa" = "oa21cd",
+                    "lsoa" = "lsoa21nm",
+                    "msoa" = "msoa21nm",
+                    "lad" = "lad22nm")
+
+  area_lookup %>%
+    distinct(.data[[area_dict[[area_type]]]]) %>%
+    pull()
+}
