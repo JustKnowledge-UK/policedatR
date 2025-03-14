155ccd9598688af0cc445b9a875befc9ae3f14bd
JolyonJoseph <jmileswilson@gmail.com>
2025-03-14 15:31:30 +0000

    add v1 for get_msoa_geometries

--- R/get_geometries.R
+++ R/get_geometries.R
@@ -65,7 +65,7 @@ get_lad_geometries <- function(subset = NULL){
 
     t1 <- Sys.time()
     cat("\nStarting request")
-    response <- fetch_data(where_clause)  # First call fetches from API
+    response <- fetch_data(where_clause)
     t2 <- Sys.time()
     time_elapsed <- t2 - t1
     cat(paste0("\nRequest done in: ", time_elapsed, " seconds"))
@@ -121,6 +121,136 @@ get_lad_geometries <- function(subset = NULL){
 
 
   return(lad_geometries)
+
+}
+
+
+
+get_msoa_geometries <- function(subset = NULL){
+
+  # API endpoint
+  base_url <- "https://services1.arcgis.com/ESMARspQHYMw9BZ9/ArcGIS/rest/services/Middle_layer_Super_Output_Areas_December_2021_Boundaries_EW_BFC_V7/FeatureServer/0/query"
+
+  # Create cache directory - but ask user to agree
+  policedatR::caching_check()
+  caching <- Sys.getenv("caching")
+  cache_dir <- Sys.getenv("cache_dir")
+
+  # Specify the request as a function so it can be memoised
+  # Note we need to do something with the status code so that non-200 responses
+  # don't get accepted and memoised. I've got a solution but not yet implemented
+  fetch_data <- function(where_clause,
+                         result_offset,
+                         max_records # GeoPortal max records in single response is 2000
+                         ) {
+    params <- list(
+      where = where_clause,  # Retrieve all records
+      outFields =  "*", # "*" specifies all. I tried specifying fewer outfields but it doesn't speed things up. No need to specify geometry as this is assumed by default
+      outSR = "4326",
+      f = "geojson",
+      resultOffset = result_offset, # for pagination
+      resultRecordCount = max_records
+    )
+
+    response <- httr::GET(base_url, query = params)
+    if (httr::status_code(response) == 200) {
+      #content(response, "text")  # Convert response to text for caching
+      return(response)
+    }
+    else {
+      stop(paste("Error:", httr::status_code(response)))
+    }
+  }
+
+
+  if(caching){
+    cd = cachem::cache_disk(cache_dir, evict = "lru")
+    # Memoise the function
+    fetch_data <- memoise::memoise(fetch_data, cache = cd)
+  }
+
+  # Initialise
+  all_results <- list()
+  result_offset <- 0
+  max_records <- 2000
+
+  # Subset options. Build on this later
+  if(is.null(subset)){
+    # If no subset has been requested, acquire all data
+    # Here pagination is required as max records in single response is 2000
+    where_clause <- "1=1" # we could build options here
+
+    t1 <- Sys.time()
+    cat("\nStarting request.")
+    # Repeat the request in 2000 record batches
+    repeat{
+
+      response <- fetch_data(where_clause, result_offset, max_records)
+      geojson_data <- httr::content(response, as = "text")
+      # Translate to sf object
+      sf_chunk <- sf::st_read(geojson_data, quiet = TRUE)
+      # Stop if no more records
+      if (nrow(sf_chunk) == 0) break
+      # Append results
+      all_results <- append(all_results, list(sf_chunk))
+      result_offset <- result_offset + max_records
+    }
+    t2 <- Sys.time()
+    time_elapsed <- t2 - t1
+    cat(paste0("\nRequest done in: ", time_elapsed, " seconds"))
+    cat("\nNote time includes local data processing")
+
+    msoa_geometries <- dplyr::bind_rows(all_results) %>%
+      # Tidy up
+      janitor::clean_names() %>%
+      dplyr::select(msoa21cd, msoa21nm, shape_area, geometry)
+  }
+  else{
+    # If a subset has been requested, run in chunks of 90. This is limit set by API
+
+    # Initialise and specify parameters for the query
+    all_results <- list()
+    batch_size <- 90
+
+    target_variable <- toupper(names(subset)[1]) # Use toupper because that is how it is on API
+    target_values <- subset[[1]]
+
+    chunks <- split(target_values, ceiling(seq_along(target_values) / batch_size))
+
+    t1 <- Sys.time()
+    cat("\nStarting request")
+    for(chunk in chunks){
+      # Update the where clause with the next batch
+      where_clause <- paste0(target_variable, " IN ('", paste(chunk, collapse = "', '"), "')")
+
+      # Run the query
+      response <- fetch_data(where_clause,
+                             result_offset, # redundant here but function requires
+                             max_records # redundant here but function requires
+                             )
+
+      # Get the contents
+      geojson_data <- httr::content(response, as = "text")
+
+      # Translate to sf object
+      sf_chunk <- sf::st_read(geojson_data, quiet = TRUE)
+
+      # Append results
+      all_results <- append(all_results, list(sf_chunk))
+
+    }
+    t2 <- Sys.time()
+    time_elapsed <- t2 - t1
+    cat(paste0("\nRequest done in: ", time_elapsed, " seconds"))
+
+    msoa_geometries <- dplyr::bind_rows(all_results) %>%
+      # Tidy up
+      janitor::clean_names() %>%
+      dplyr::select(msoa21cd, msoa21nm, shape_area, geometry)
+  }
+
+
+  return(msoa_geometries)
 
 }
 
