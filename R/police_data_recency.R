ce32066c3babc50d7758be88778be8d612e3b1f3
JolyonJoseph <jmileswilson@gmail.com>
2025-05-08 14:13:06 +0100

    add error handling for api timeouts

--- R/police_data_recency.R
+++ R/police_data_recency.R
@@ -8,19 +8,37 @@
 #'
 #' newest_data()
 #'
-newest_data <- function(){
-  oldest <- httr::GET("https://data.police.uk/api/crimes-street-dates")
-  k <- httr::content(oldest)
-  l <- lapply(k, unlist)
-  m <- do.call(dplyr::bind_rows, l)
+newest_data <- function(timeout = 10){
 
-  # format as date to be able to use min/max functions
-  for(i in 1:nrow(m)){
-    m$date[i] <- paste0(m$date[i],"-01")
+  newest <- tryCatch({
+    R.utils::withTimeout({
+      httr::GET("https://data.police.uk/api/crimes-street-dates", httr::timeout(timeout))
+    }, timeout = timeout)  # Allow slightly longer than GET timeout, just in case
+  }, TimeoutException = function(e) {
+    message("Timed out")
+    return(NULL)
+  }, error = function(e) {
+    message(e$message)
+    return(NULL)
+  })
+
+  if(!is.null(newest)){
+    if(httr::status_code(newest) == 200){
+      k <- httr::content(newest)
+      l <- lapply(k, unlist)
+      m <- do.call(dplyr::bind_rows, l)
+      m$date <- as.Date(paste0(m$date, "-01"))
+      newest_date <- substr(max(m$date), 1, 7)
+      message("Most recent month acquired from API")
+    }
   }
-  m$date <- as.Date(m$date)
+  else{
+    this_date <- Sys.Date()
+    newest_date <- format(seq(this_date, length = 2, by = "-3 months")[2], "%Y-%m")
+    message("Warning: API communication didn't work. Assuming most recent month is this month - 3.")
+  }
 
-  return(substr(max(m$date), 1, 7))
+  return(newest_date)
 
 }
 
@@ -35,18 +53,36 @@ newest_data <- function(){
 #' oldest_data()
 #'
 #'
-oldest_data <- function(){
-  oldest <- httr::GET("https://data.police.uk/api/crimes-street-dates")
-  k <- httr::content(oldest)
-  l <- lapply(k, unlist)
-  m <- do.call(dplyr::bind_rows, l)
+oldest_data <- function(timeout = 10){
 
-  # format as date to be able to use min/max functions
-  for(i in 1:nrow(m)){
-    m$date[i] <- paste0(m$date[i],"-01")
+  oldest <- tryCatch({
+    R.utils::withTimeout({
+      httr::GET("https://data.police.uk/api/crimes-street-dates", httr::timeout(timeout))
+    }, timeout = timeout)  # Allow slightly longer than GET timeout, just in case
+  }, TimeoutException = function(e) {
+    message("Timed out")
+    return(NULL)
+  }, error = function(e) {
+    message(e$message)
+    return(NULL)
+  })
+
+  if(!is.null(oldest)){
+    if(httr::status_code(oldest) == 200){
+      k <- httr::content(oldest)
+      l <- lapply(k, unlist)
+      m <- do.call(dplyr::bind_rows, l)
+      m$date <- as.Date(paste0(m$date, "-01"))
+      oldest_date <- substr(min(m$date), 1, 7)
+      message("Most recent month acquired from API")
+    }
   }
-  m$date <- as.Date(m$date)
+  else{
+    this_date <- Sys.Date()
+    oldest_date <- format(seq(this_date, length = 2, by = "-39 months")[2], "%Y-%m")
+    message("Warning: API communication didn't work. Assuming oldest month is this month - 39.")
+  }
 
-  return(substr(min(m$date), 1, 7))
+  return(oldest_date)
 
 }
