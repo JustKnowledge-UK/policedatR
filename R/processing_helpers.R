3a5b5474b4a6e8062e3a97fbb0fa52436ef1a437
JolyonJoseph <jmileswilson@gmail.com>
2025-05-20 15:07:00 +0100

    add riskratio_from_df to processing_helpers.R

--- R/processing_helpers.R
+++ R/processing_helpers.R
@@ -279,5 +279,37 @@ show_analysis_variables <- function(){
   )
 }
 
+#' Risk ratio from data frame
+#'
+#' Calculates relative risk ratio and confidence intervals from a
+#' contingency table constructed for analysis of stop and search disparities
+#'
+#' @param df Contingency table as data frame on which to calculate risk ratio
+#' @param name Desired name of resultant data frame containing statistics
+#'
+#' @return A data frame containing risk ratio and confidence intervals
+#'
+#' @keywords internal
+#'
+#' @examples
+#'
+#' rr <- riskratio_from_df(df, "Stop and Search disparity")
+#'
+riskratio_from_df <- function(df){
+
+  mat <- df %>%
+    dplyr::select(-ethnicity) %>%
+    as.matrix()
+
+  rr <- epitools::riskratio(mat) # run
+
+  # output into formatted data frame
+  df_out <- data.frame("rr" = rr[["measure"]][2,1],
+                       "ci_low" = rr[["measure"]][2,2],
+                       "ci_upp" = rr[["measure"]][2,3])
+  return(df_out)
+}
+
+
 
 
